c2664ed1636313add57242771e3cd92dac51a416
Thales do Prado <111712557+jothas2000@users.noreply.github.com>
2025-02-17 09:14:10 -0300

    Delete EngenhariaProjeto_VersãoFinal directory

--- EngenhariaProjeto_VersãoFinal/JS/proxFase1.js
+++
@@ -1,5 +0,0 @@
-function nextPhase() {
-    alert("Indo para a próxima fase...");
-    // Aqui você pode redirecionar para a próxima fase do jogo
-    window.location.href = '../HTML/Memoria.html';
-}
--- EngenhariaProjeto_VersãoFinal/JS/proxFase2.js
+++
@@ -1,4 +0,0 @@
-function nextPhase() {
-    // Aqui você pode redirecionar para a próxima fase do jogo
-    window.location.href = '../HTML/pontuacao.php';
-}
